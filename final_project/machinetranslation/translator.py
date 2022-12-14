53ea9c4d934aed79e71f1ec61c2e4453947d4d84
Joguima24 <j.gui@live.com.pt>
2022-12-14 15:51:32 -0500

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -18,13 +18,13 @@ language_translator.set_service_url(url)
 language_translator.set_disable_ssl_verification(True)
 
 def english_to_french(english_text):
-    french_text = language_translator.translate(
+    french_text_res = language_translator.translate(
     text=english_text,
     model_id='en-fr').get_result()
-    return french_text
+    return french_text_res
 
 def french_to_english(french_text):
-    english_text = language_translator.translate(
+    english_text_res = language_translator.translate(
     text=french_text,
     model_id='fr-en').get_result()
-    return english_text
+    return english_text_res
